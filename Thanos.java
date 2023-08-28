16c1a0b95e838553fd61462fce7b9429a8b32a69
snjezana.prgic@telekom.de <snjezana.prgic@telekom.de>
2023-08-28 16:07:35 +0200

    Thanos

---
+++ Thanos.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Thanos {
+
+    public static void main(String[] args) {
+        ArrayList<Hero> heroes = new ArrayList<>();
+
+        heroes.add(new Hero("Black Widow", 34));
+        heroes.add(new Hero("Captain America", 100));
+        heroes.add(new Hero("Vision", 3));
+        heroes.add(new Hero("Iron Man", 48));
+        heroes.add(new Hero("Scarlet Witch", 29));
+        heroes.add(new Hero("Thor", 1500));
+        heroes.add(new Hero("Hulk", 49));
+        heroes.add(new Hero("Doctor Strange", 42));
+
+        for (Hero hero : heroes) {
+            if ("Thor".equals(hero.getName())) {
+                hero.setAge(1501);
+                break;
+            }
+        }
+
+        Collections.shuffle(heroes);
+
+        int halfSize = heroes.size()/2;
+        List<Hero> topHalf = new ArrayList<>(heroes.subList(0,halfSize));
+
+
+        heroes.clear();
+        heroes.addAll(topHalf);
+
+        for (Hero hero : heroes) {
+            System.out.println(hero);
+
+        }
+        for (Hero hero : heroes) {
+            System.out.println(hero.getName());
+        }
+
+        }
+    }
+
+
